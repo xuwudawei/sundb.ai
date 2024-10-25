5afba474b3983b700ae6581f8ddf69aff3135e16
xuwudawei <xuwudawei100@gmail.com>
2024-10-25 10:36:36 +0800

    test update

--- backend/app/utils/dspy.py
+++ backend/app/utils/dspy.py
@@ -21,7 +21,7 @@ def get_dspy_lm_by_llama_llm(llama_llm: BaseLLM) -> dspy.LM:
     Get the dspy LM by the llama LLM.
 
     In this project, we use both llama-index and dspy, both of them have their own LLM implementation.
-    This function can help us reduce the complexity of the code by converting the llama LLM to the dspy LLM.
+    This function can help us reduce the complexity of the code by converting the llama LLM to the dspy LLM easily.
     """
     if type(llama_llm) is OpenAI:
         return dspy.OpenAI(
