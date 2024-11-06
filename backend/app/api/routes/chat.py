864f44f9ddd3d7e573bb742d7d9f701727d7491b
xuwudawei <xuwudawei100@gmail.com>
2024-11-06 13:00:51 +0800

    refactor(api): add logging for exception in chat route

--- backend/app/api/routes/chat.py
+++ backend/app/api/routes/chat.py
@@ -1,3 +1,4 @@
+import logging
 from uuid import UUID
 from typing import List, Optional
 from http import HTTPStatus
@@ -29,6 +30,8 @@ from app.rag.types import (
     ChatMessageSate,
 )
 from app.exceptions import ChatNotFound
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -79,6 +82,7 @@ def chats(
     except ChatNotFound:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Chat not found")
     except Exception as e:
+        logger.exception(e)
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
         )
